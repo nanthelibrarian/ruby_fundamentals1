3998afde847b924773adfe02ab0af42b708cf9f3
Nancy Wood <nancywood13@gmail.com>
2015-06-09 16:35:45 -0400

    counting from 1 to 100

---
+++ exercise4.rb
@@ -0,0 +1,9 @@
+# numbers from 1 to 100
+# For multiples of three, output "Bit" instead of the number 
+# for multiples of five, output "Maker". 
+# For numbers which are multiples of both three and five, output "BitMaker".
+
+one_to_100 = (1..100)
+one_to_100.each do |num|
+puts num 
+end 
