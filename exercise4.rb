29b3a0d75b992a2c343a6a2e8ff21f9ba534de60
Nancy Wood <nancywood13@gmail.com>
2015-06-09 16:50:05 -0400

    counts 1 to 100 replacing divisible by 5 and 3 with Bitmaker, divisible by 3 with Bit, divisible by 5 with maker

--- exercise4.rb
+++ exercise4.rb
@@ -4,6 +4,17 @@
 # For numbers which are multiples of both three and five, output "BitMaker".
 
 one_to_100 = (1..100)
+
 one_to_100.each do |num|
-puts num 
-end 
+	if num % 3 == 0 && num % 5 == 0
+		puts "BitMaker"
+	elsif num % 3 == 0
+	    puts "Bit" 
+	elsif num % 5 == 0  
+	  	puts "Maker" 
+	else 
+		puts num 
+	end 
+end
+
+ 
