d7b27313e58514cd4de983d33fc56f140e16db60
Nancy Wood <nancywood13@gmail.com>
2015-06-09 15:20:39 -0400

    second exercise completed

---
+++ exercise2.rb
@@ -0,0 +1,10 @@
+# basic tip calculator for non-mathletes
+x = 55
+tip = x * 0.2
+puts "please tip " << tip.to_s
+
+# 45628 multiplied by 7839 output the result with string interpolation
+puts "the product of 45628 multiplied by 7839 is #{45628 * 7839}"
+
+
+
